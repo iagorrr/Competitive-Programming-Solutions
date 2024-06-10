9bf526e694910f07041832b0d668f50997c33214
iagorrr <iagorrr04@gmail.com>
2024-06-10 11:59:02 -0300

    ad-hoc

---
+++ submissions/atCoder/abc217_a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int32_t main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	string s, t;
+	cin >> s >> t;
+	if (s < t) {
+		cout << "Yes\n";
+	}
+	else 
+		cout << "No\n";
+}
+
+// AC, ad-hoc
+
