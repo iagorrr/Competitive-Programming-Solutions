6f4783ec6962c875da38106dd057df0a70666f24
iagorrr <iagorrr04@gmail.com>
2024-06-24 11:18:18 -0300

    ad-hoc

---
+++ submissions/Maratonas DF/Seletiva UnB 2024/D. Dogsay.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+int32_t main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	string s;
+	getline(cin, s);
+
+	cout << ' ' << string(s.size()+2, '_') <<'\n';
+	cout << "< " << s << " >\n";
+	cout << ' ' << string(s.size()+2, '=') <<'\n';
+	
+	cout << "        \\  ^..^      /\n";
+	cout << "         \\ /_/\\_____/\n";
+	cout << "              /\\   /\\\n";
+	cout << "             /  \\ /  \\\n";
+}
+
+// AC, ad-hoc
+
