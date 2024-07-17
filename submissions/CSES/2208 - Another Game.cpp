3e0413da160caab675ea46fa0386511be22f36c2
iagorrr <iagorrr04@gmail.com>
2024-07-17 15:06:09 -0300

    game theory

---
+++ submissions/CSES/2208 - Another Game.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int32_t main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int t;
+	cin >> t;
+
+	while (t--) {
+		int n;
+		cin >> n;
+
+		int anyodd=0;
+		while(n--) {
+			int x;
+			cin >> x;
+			anyodd |= (x&1);
+		}
+
+		if (anyodd) cout << "first\n";
+		else cout << "second\n";
+	}
+}
+
+// AC, game theory
