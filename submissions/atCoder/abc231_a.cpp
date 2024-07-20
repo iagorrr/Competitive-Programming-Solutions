da4363cefe56f2a0fdecb30b60a5a7e31d381eeb
iagorrr <iagorrr04@gmail.com>
2024-07-20 11:14:21 -0300

    ad-hoc

---
+++ submissions/atCoder/abc231_a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int32_t main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	using ld = long double;
+	ld a;
+	cin >> a;
+
+	a = a / (ld)100;
+
+	cout << fixed << setprecision(10) << a << '\n';
+}
+
+
+// AC, ad-hoc
