607f890d8cc726144eddfec628219f1b499b3eda
iagorrr <iagorrr04@gmail.com>
2024-07-26 19:56:09 -0300

    ad-hoc

---
+++ submissions/atCoder/abc232_a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int32_t main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	char a,b,c;
+	cin >> a >> b >> c;
+
+	a-=48;
+	c-=48;
+	cout << a *c<< '\n';
+}
+
+
+// AC, ad-hoc
