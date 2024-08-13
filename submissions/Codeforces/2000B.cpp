649c86b4fb57b822f624b893b7f5620718bfc726
iagorrr <iagorrr04@gmail.com>
2024-08-13 19:45:36 -0300

    implementation

---
+++ submissions/Codeforces/2000B.cpp
@@ -0,0 +1,89 @@
+/*{=======================~ BEGIN MACRO ~================================*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+#include "debug.cpp"
+#else
+#define dbg(...)
+#endif
+
+#define endl '\n'
+
+#define fastio                        \
+        ios_base::sync_with_stdio(0); \
+        cin.tie(0);
+
+#define int long long
+
+#define all(j) j.begin(), j.end()
+#define rall(j) j.rbegin(), j.rend()
+#define len(j) (int)j.size()
+#define rep(i, a, b) \
+        for (common_type_t<decltype(a), decltype(b)> i = a; i < b; i++)
+#define rrep(i, a, b) \
+        for (common_type_t<decltype(a), decltype(b)> i = a; i > b; i--)
+#define pb push_back
+#define pf push_front
+#define eb emplace_back
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vll2d = vector<vll>;
+using vi = vector<int>;
+using vi2d = vector<vi>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+using vc = vector<char>;
+using vs = vector<string>;
+
+template<typename T, typename U> inline bool chmax(T &a, U const& b) {
+       	return (a < b ? a = b, 1 : 0); }
+template<typename T, typename U> inline bool chmin(T &a, U const& b) {
+       	return (a > b ? a = b, 1 : 0); }
+
+/*=========================~ END MACRO ~================================}*/
+
+auto run() {
+	int N;
+	cin>>N;
+	vi XS(N);
+	for(auto&xi:XS)cin>>xi;
+
+	vi occ(N+2);
+	bool ok=true;
+	dbg(N,XS);
+	rep(i,0,N){
+		auto xi=XS[i];
+		ok&=(!i||occ[xi-1]||occ[xi+1]);
+		occ[xi]=1;
+	}
+
+	if(ok)cout<<"YES\n";
+	else cout<<"NO\n";
+}
+
+int32_t main() {
+#ifndef LOCAL
+        fastio;
+#endif
+
+        int T = 1;
+
+        cin >> T;
+
+        rep(t, 0, T) {
+                dbg(t);
+                run();
+        }
+}
+
+/*{
+  AC, implementation
+  }*/
+
+
