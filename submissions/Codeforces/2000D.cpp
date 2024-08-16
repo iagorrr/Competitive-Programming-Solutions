4ec949d56948a51751d897f26c6aff0d0bbb5a46
iagorrr <iagorrr04@gmail.com>
2024-08-16 12:09:42 -0300

    greddy, binary search

--- submissions/Codeforces/2000D.cpp
+++ submissions/Codeforces/2000D.cpp
@@ -1,5 +1,4 @@
-/*{=======================~ BEGIN MACRO ~================================*/
-
+/*{=============~ BEGIN TEMPLATE ~==============*/
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -11,22 +10,28 @@ using namespace std;
 
 #define endl '\n'
 
-#define fastio                        \
-        ios_base::sync_with_stdio(0); \
-        cin.tie(0);
+#define fastio                  \
+  ios_base::sync_with_stdio(0); \
+  cin.tie(0);
 
 #define int long long
 
 #define all(j) j.begin(), j.end()
 #define rall(j) j.rbegin(), j.rend()
 #define len(j) (int)j.size()
-#define rep(i, a, b) \
-        for (common_type_t<decltype(a), decltype(b)> i = a; i < b; i++)
-#define rrep(i, a, b) \
-        for (common_type_t<decltype(a), decltype(b)> i = a; i > b; i--)
+#define rep(i, a, b)                           \
+  for (common_type_t<decltype(a), decltype(b)> \
+           i = a;                              \
+       i < b; i++)
+#define rrep(i, a, b)                          \
+  for (common_type_t<decltype(a), decltype(b)> \
+           i = a;                              \
+       i > b; i--)
 #define pb push_back
 #define pf push_front
 #define eb emplace_back
+#define lb lower_bound
+#define ub upper_bound
 
 using ll = long long;
 using ull = unsigned long long;
@@ -37,70 +42,71 @@ using vll2d = vector<vll>;
 using vi = vector<int>;
 using vi2d = vector<vi>;
 using pii = pair<int, int>;
-using vii = vector<pii>;
+using vpii = vector<pii>;
 using vc = vector<char>;
 using vs = vector<string>;
 
-template<typename T, typename U> inline bool chmax(T &a, U const& b) {
-       	return (a < b ? a = b, 1 : 0); }
-template<typename T, typename U> inline bool chmin(T &a, U const& b) {
-       	return (a > b ? a = b, 1 : 0); }
-
-/*=========================~ END MACRO ~================================}*/
-
-auto run() {
-	int N;
-	cin>>N;
-	vll XS(N);
-	for(auto&xi:XS)
-		cin>>xi;
-
-
-	string S;
-	cin>>S;
-
-	vi pl, pr;
-	rep(i,0,N){
-		if(S[i]=='L')pl.eb(i);
-		else pr.eb(i);
-	}
-
-	vll psum(N);
-	psum[0]=XS[0];
-	rep(i,1,N){
-		psum[i]=XS[i]+psum[i-1];
-	}
-
-	reverse(all(pl));
-	ll ans=0;
-	
-	while(len(pl)&&len(pr)){
-		int l=pl.back();
-		int r=pr.back();
-		if(r<l)break;
-		ans+=(psum[r]-(l?psum[l-1]:0));
-		pl.pop_back(),pr.pop_back();
-	}
-	cout<<ans<<endl;
+template <typename T, typename U>
+inline bool chmax(T &a, U const &b) {
+  return (a < b ? a = b, 1 : 0);
 }
+template <typename T, typename U>
+inline bool chmin(T &a, U const &b) {
+  return (a > b ? a = b, 1 : 0);
+}
+
+void run();
 
 int32_t main() {
 #ifndef LOCAL
-        fastio;
+  fastio;
 #endif
 
-        int T = 1;
+  int T = 1;
 
-        cin >> T;
+  cin >> T;
 
-        rep(t, 0, T) {
-                dbg(t);
-                run();
-        }
+  rep(t, 0, T) {
+    dbg(t);
+    run();
+  }
 }
 
+/*===============~ END TEMPLATE ~==============}*/
+
 /*{
-  AC, greddy, prefix sum
-  }*/
-
-
+  AC, greddy, binary search
+}*/
+const int oo=1'000'000'000;
+void run() {
+  int N,Q;cin>>N>>Q;
+  map<string,vector<int>> pos;
+  vs PS(N);
+  rep(i,0,N){
+    string s;cin>>s;
+    if(s[0]>s[1])swap(s[0],s[1]);
+    pos[s].eb(i);
+    PS[i]=s;
+  }
+  rep(q,0,Q){
+    int l,r;cin>>l>>r;l--,r--;
+    if(l>r)swap(l,r);
+    auto sl=PS[l],sr=PS[r];
+    int ans=oo;
+    rep(i,0,2){
+      rep(j,0,2){
+        string s;s.pb(sl[i]),s.pb(sr[j]);
+        if(s[0]>s[1])swap(s[0],s[1]);
+        auto it=ub(all(pos[s]),r);
+        if(it!=pos[s].end()){
+          chmin(ans,abs(l-*it)+abs(r-*it));
+        }
+        if(it!=pos[s].begin()){
+          it--;
+          chmin(ans,abs(l-*it)+abs(r-*it));
+        }
+      }
+    }
+    cout<<(ans==oo?-1:ans)<<endl;
+  }
+}
