0e5400034ed1641985701df12727ae6685261160
iagorrr <iagorrr04@gmail.com>
2024-08-23 20:07:41 -0300

    data structures

---
+++ submissions/atCoder/abc240_c.cpp
@@ -0,0 +1,106 @@
+/*8<{==========~ BEGIN TEMPLATE ~============>8*/
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef LOCAL
+#include "debug.cpp"
+#else
+#define dbg(...)
+#endif
+#define endl '\n'
+#define fastio                  \
+  ios_base::sync_with_stdio(0); \
+  cin.tie(0);
+#define int long long
+#define all(j) j.begin(), j.end()
+#define rall(j) j.rbegin(), j.rend()
+#define len(j) (int)j.size()
+#define rep(i, a, b)                           \
+  for (common_type_t<decltype(a), decltype(b)> \
+           i = (a);                            \
+       i < (b); i++)
+#define rrep(i, a, b)                          \
+  for (common_type_t<decltype(a), decltype(b)> \
+           i = (a);                            \
+       i > (b); i--)
+#define trav(a, x) for (auto &a : x)
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define eb emplace_back
+#define lb lower_bound
+#define ub upper_bound
+#define emp emplace
+#define ins insert
+#define divc(a, b) ((a) + (b)-1ll) / (b)
+using str = string;
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vll2d = vector<vll>;
+using vi = vector<int>;
+using vi2d = vector<vi>;
+using pii = pair<int, int>;
+using vpii = vector<pii>;
+using vc = vector<char>;
+using vs = vector<str>;
+template <typename T, typename T2>
+using umap = unordered_map<T, T2>;
+template <typename T>
+using pqmn =
+    priority_queue<T, vector<T>, greater<T>>;
+template <typename T>
+using pqmx = priority_queue<T, vector<T>>;
+template <typename T, typename U>
+inline bool chmax(T &a, U const &b) {
+  return (a < b ? a = b, 1 : 0);
+}
+template <typename T, typename U>
+inline bool chmin(T &a, U const &b) {
+  return (a > b ? a = b, 1 : 0);
+}
+/*8<============~ END TEMPLATE ~============}>8*/
+void run();
+
+int32_t main() {
+#ifndef LOCAL
+  fastio;
+#endif
+
+  int T = 1;
+
+  // cin >> T;
+
+  rep(t, 0, T) {
+    dbg(t);
+    run();
+  }
+}
+
+/*8<
+>8*/
+void run() {
+  int N,X;cin>>N>>X;
+  vpii XS(N);
+  for(auto&[a,b]:XS)cin>>a>>b;
+  vi pos(X+1);
+  pos[0]=1;
+  for(auto&[a,b]:XS){
+    vi pos2(X+1);
+    rrep(i,X,0-1){
+      if(i-a>=0)
+      pos2[i]|=pos[i-a];
+      if(i-b>=0)
+      pos2[i]|=pos[i-b];
+    }
+    pos=pos2;
+  }
+  if(pos[X]){
+    cout<<"Yes"<<endl;
+  }
+  else cout<<"No"<<endl;
+}
+
+// AC, data structures
