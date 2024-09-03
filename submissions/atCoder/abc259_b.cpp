512aa6e2deb7da1b8aac2ff04379aa5f1776c509
iagorrr <iagorrr04@gmail.com>
2024-09-03 16:20:34 -0300

    geometry

---
+++ submissions/atCoder/abc259_b.cpp
@@ -0,0 +1,152 @@
+/*8<{==========~ BEGIN TEMPLATE ~============>8*/
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef LOCAL
+#include "debug.cpp"
+#else
+#define dbg(...)
+#endif
+#define endl '\n'
+#define fastio                  \
+  ios_base::sync_with_stdio(0); \
+  cin.tie(0);
+#define all(j) j.begin(), j.end()
+#define rall(j) j.rbegin(), j.rend()
+#define len(j) (int)j.size()
+#define rep(i, a, b)                           \
+  for (common_type_t<decltype(a), decltype(b)> \
+           i = (a);                            \
+       i < (b); i++)
+#define rrep(i, a, b)                          \
+  for (common_type_t<decltype(a), decltype(b)> \
+           i = (a);                            \
+       i > (b); i--)
+#define trav(xi, xs) for (auto &xi : xs)
+#define rtrav(xi, xs) \
+  for (auto &xi : ranges::views::reverse(xs))
+using ll = long long;
+#define inte ll
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define eb emplace_back
+#define lb lower_bound
+#define ub upper_bound
+#define fi first
+#define se second
+#define emp emplace
+#define ins insert
+#define divc(a, b) ((a) + (b)-1ll) / (b)
+using str = string;
+using ull = unsigned long long;
+using ld = long double;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vll2d = vector<vll>;
+using vi = vector<int>;
+using vi2d = vector<vi>;
+using pii = pair<int, int>;
+using vpii = vector<pii>;
+using vc = vector<char>;
+using vs = vector<str>;
+template <typename T, typename T2>
+using umap = unordered_map<T, T2>;
+template <typename T>
+using pqmn =
+    priority_queue<T, vector<T>, greater<T>>;
+template <typename T>
+using pqmx = priority_queue<T, vector<T>>;
+template <typename T, typename U>
+inline bool chmax(T &a, U const &b) {
+  return (a < b ? a = b, 1 : 0);
+}
+template <typename T, typename U>
+inline bool chmin(T &a, U const &b) {
+  return (a > b ? a = b, 1 : 0);
+}
+/*8<============~ END TEMPLATE ~============}>8*/
+void run();
+
+int32_t main() {
+#ifndef LOCAL
+  fastio;
+#endif
+
+  int T = 1;
+
+  //cin >> T;
+
+  rep(t, 0, T) {
+    dbg(t);
+    run();
+  }
+}
+
+template <class T>
+int sgn(T x) {
+  return (x > 0) - (x < 0);
+}
+template <class T>
+struct Point {
+  typedef Point P;
+  T x, y;
+  explicit Point(T x = 0, T y = 0) : x(x), y(y) {}
+  bool operator<(P p) const {
+    return tie(x, y) < tie(p.x, p.y);
+  }
+  bool operator==(P p) const {
+    return tie(x, y) == tie(p.x, p.y);
+  }
+  P operator+(P p) const {
+    return P(x + p.x, y + p.y);
+  }
+  P operator-(P p) const {
+    return P(x - p.x, y - p.y);
+  }
+  P operator*(T d) const {
+    return P(x * d, y * d);
+  }
+  P operator/(T d) const {
+    return P(x / d, y / d);
+  }
+  T dot(P p) const { return x * p.x + y * p.y; }
+  T cross(P p) const { return x * p.y - y * p.x; }
+  T cross(P a, P b) const {
+    return (a - *this).cross(b - *this);
+  }
+  T dist2() const { return x * x + y * y; }
+  double dist() const {
+    return sqrt((double)dist2());
+  }
+  // angle to x-axis in interval [-pi, pi]
+  double angle() const { return atan2(y, x); }
+  P unit() const {
+    return *this / dist();
+  }  // makes dist()=1
+  P perp() const {
+    return P(-y, x);
+  }  // rotates +90 degrees
+  P normal() const { return perp().unit(); }
+  // returns point rotated 'a' radians ccw around
+  // the origin
+  P rotate(double a) const {
+    return P(x * cos(a) - y * sin(a),
+             x * sin(a) + y * cos(a));
+  }
+  friend ostream& operator<<(ostream& os, P p) {
+    return os << "(" << p.x << "," << p.y << ")";
+  }
+};
+void run() {
+  int A,B,D;cin>>A>>B>>D;
+  auto p=Point((ld)A,(ld)B);
+  ld PI=acos(-1);
+  auto ans=p.rotate(D*(PI/180));
+  cout<<fixed<<setprecision(10)<<ans.x<<' '<<ans.y<<endl;
+
+}
+
+/*8<
+  AC, geometry
+>8*/
