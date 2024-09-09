dbc5eea593199cf0342805393290846bcb73a4b5
iagorrr <iagorrr04@gmail.com>
2024-09-09 14:42:04 -0300

    geometry

---
+++ submissions/Codeforces/1047B.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int32_t main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  int n;
+  cin >> n;
+  int ans = 0;
+  for (int i = 0; i < n; i++) {
+    int x, y;
+    cin >> x >> y;
+    ans = max(ans, y + x);
+  }
+  cout << ans << endl;
+}
