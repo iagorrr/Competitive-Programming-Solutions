9b101738256b0be7cfc9f4c417ddab5c64ae0524
iagorrr <iagorrr04@gmail.com>
2024-09-10 16:36:33 -0300

    geometry

---
+++ submissions/Codeforces/1312A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void run() {
+  int n, m;
+  cin >> n >> m;
+  cout << (n % m ? "NO" : "YES") << '\n';
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin >> t;
+  while (t--) {
+    run();
+  }
+}
+
+// AC, geometry
