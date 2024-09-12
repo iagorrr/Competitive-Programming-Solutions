551496badba628d7a6cdcb9e1a88588facb921e8
iagorrr <iagorrr04@gmail.com>
2024-09-12 15:21:10 -0300

    geometry

---
+++ submissions/atCoder/abc191_d.cpp
@@ -0,0 +1,104 @@
+/*8<{==========~ BEGIN TEMPLATE ~============>8*/
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef LOCAL
+#include "debug.cpp"
+#else
+#define dbg(...)
+#endif
+#define endl '\n'
+#define fastio                  \
+  ios_base::sync_with_stdio(0); \
+  cin.tie(0);
+#define all(j) j.begin(), j.end()
+#define rall(j) j.rbegin(), j.rend()
+#define len(j) (int)j.size()
+#define rep(i, a, b)                           \
+  for (common_type_t<decltype(a), decltype(b)> \
+           i = (a);                            \
+       i < (b); i++)
+#define rrep(i, a, b)                          \
+  for (common_type_t<decltype(a), decltype(b)> \
+           i = (a);                            \
+       i > (b); i--)
+#define trav(xi, xs) for (auto &xi : xs)
+#define rtrav(xi, xs) \
+  for (auto &xi : ranges::views::reverse(xs))
+using ll = long long;
+//#define int ll
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define eb emplace_back
+#define lb lower_bound
+#define ub upper_bound
+#define fi first
+#define se second
+#define emp emplace
+#define ins insert
+#define divc(a, b) ((a) + (b)-1ll) / (b)
+using str = string;
+using ull = unsigned long long;
+using ld = long double;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vll2d = vector<vll>;
+using vi = vector<int>;
+using vi2d = vector<vi>;
+using pii = pair<int, int>;
+using vpii = vector<pii>;
+using vc = vector<char>;
+using vs = vector<str>;
+template <typename T, typename T2>
+using umap = unordered_map<T, T2>;
+template <typename T>
+using pqmn =
+    priority_queue<T, vector<T>, greater<T>>;
+template <typename T>
+using pqmx = priority_queue<T, vector<T>>;
+template <typename T, typename U>
+inline bool chmax(T &a, U const &b) {
+  return (a < b ? a = b, 1 : 0);
+}
+template <typename T, typename U>
+inline bool chmin(T &a, U const &b) {
+  return (a > b ? a = b, 1 : 0);
+}
+/*8<============~ END TEMPLATE ~============}>8*/
+
+const ll f=10000;
+
+ll read() {
+  double x;
+  cin>>x;
+  return llround(x*f);
+}
+
+template <typename T1, typename T2>
+constexpr typename std::common_type<T1, T2>::type floor_div(T1 x, T2 y) {
+  assert(y != 0);
+  if (y < 0) x = -x, y = -y;
+  return x < 0 ? (x - y + 1) / y : x / y;
+}
+
+int32_t main(){
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  ll x0,y0,r;
+  x0=read();
+  y0=read();
+  r=read();
+  ll ans=0;
+  const ll R=2'00'000;
+  for(ll i=-R;i<=R;i++){
+    ll y=i*f;
+    ll dy=y0-y;
+    ll dx2=r*r-dy*dy;
+    if(dx2<0)continue;
+    ll dx=floor(sqrt((long double)dx2));
+    ans += floor_div(x0+dx,f) - floor_div(x0-dx-1,f);
+  }
+  cout<<ans<<'\n';
+}
+// AC, geometry
