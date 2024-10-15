fb45121ab6e0faf4f2a3e966849972b98d8ccc38
iagorrr <iagorrr04@gmail.com>
2024-10-15 15:32:03 -0300

    centroid, trees

--- submissions/Codeforces/1387B2.cpp
+++ submissions/Codeforces/1387B2.cpp
@@ -58,6 +58,7 @@ template <typename T, typename U>
 inline bool chmin(T &a, U const &b) {
   return (a > b ? a = b, 1 : 0);
 }
+using Graph = vector<vector<pair<ll, int>>>;
 /*8<============~ END TEMPLATE ~============}>8*/
 void run();
 
@@ -76,14 +77,90 @@ int32_t main() {
   }
 }
 
+/*8<
+  @Title:
+    Lowest Common Ancestor
+
+  @Description:
+
+    Given two nodes of a tree find their lowest
+    common ancestor, or their distance
+
+  @Title:
+
+    Build : $O(V)$, Queries: $O(1)$
+>8*/
+
+template <typename T>
+struct SparseTable {
+  vector<T> v;
+  int n;
+  static const int b = 30;
+  vi mask, t;
+
+  int op(int x, int y) { return v[x] < v[y] ? x : y; }
+  int msb(int x) { return __builtin_clz(1) - __builtin_clz(x); }
+  SparseTable() {}
+  SparseTable(const vector<T> &v_) : v(v_), n(v.size()), mask(n), t(n) {
+    for (int i = 0, at = 0; i < n; mask[i++] = at |= 1) {
+      at = (at << 1) & ((1 << b) - 1);
+      while (at and op(i, i - msb(at & -at)) == i) at ^= at & -at;
+    }
+    for (int i = 0; i < n / b; i++)
+      t[i] = b * i + b - 1 - msb(mask[b * i + b - 1]);
+    for (int j = 1; (1 << j) <= n / b; j++)
+      for (int i = 0; i + (1 << j) <= n / b; i++)
+        t[n / b * j + i] =
+            op(t[n / b * (j - 1) + i], t[n / b * (j - 1) + i + (1 << (j - 1))]);
+  }
+  int small(int r, int sz = b) { return r - msb(mask[r] & ((1 << sz) - 1)); }
+  T query(int l, int r) {
+    if (r - l + 1 <= b) return small(r, r - l + 1);
+    int ans = op(small(l + b - 1), small(r));
+    int x = l / b + 1, y = r / b - 1;
+    if (x <= y) {
+      int j = msb(y - x + 1);
+      ans = op(ans, op(t[n / b * j + x], t[n / b * j + y - (1 << j) + 1]));
+    }
+    return ans;
+  }
+};
+
+struct LCA {
+  SparseTable<int> st;
+  int n;
+  vi v, pos, dep;
+  vll wdep;
+
+  LCA(const Graph &g, int root) : n(len(g)), pos(n), wdep(n) {
+    dfs(root, 0, -1, g);
+    st = SparseTable<int>(vector<int>(all(dep)));
+  }
+
+  void dfs(int i, int d, int p, const Graph &g) {
+    v.eb(len(dep)) = i, pos[i] = len(dep), dep.eb(d);
+    for (auto [w, j] : g[i])
+      if (j != p) {
+        wdep[j] = wdep[i] + w;
+        dfs(j, d + 1, i, g);
+        v.eb(len(dep)) = i, dep.eb(d);
+      }
+  }
+
+  int lca(int a, int b) {
+    int l = min(pos[a], pos[b]);
+    int r = max(pos[a], pos[b]);
+    return v[st.query(l, r)];
+  }
+  ll dist(int a, int b) { return wdep[a] + wdep[b] - 2ll * wdep[lca(a, b)]; }
+
+};
 
 /*8<
   @Title: Centroid Decomposition
   @Description:
         Builds a vector $fat$ where $fat_i$ is who is the father of the node $i$
 in the centroid decomposed tree. >8*/
-//#pragma once
-//#include "../Contest/template.cpp"
 vi centroidDecomposition(const vi2d &g) {
   int n = len(g);
   vi fat(n, -1), szt(n), tk(n);
@@ -111,73 +188,54 @@ vi centroidDecomposition(const vi2d &g) {
 }
 
 
+
 void run() {
-	int N; cin >> N;
-	vi2d TREE(N);
-	rep(i, 0, N - 1) {
-		int u, v; cin >> u >> v;
+	int N;
+	cin >> N;
+
+	vi2d ADJ(N);
+	Graph ADJ2(N);
+	rep(i, 0, N -1) {
+		int u, v;
+		cin >> u >> v;
 		u--, v--;
-		TREE[u].eb(v);
-		TREE[v].eb(u);
+		ADJ[u].eb(v);
+		ADJ[v].eb(u);
+
+		ADJ2[u].eb(1,v);
+		ADJ2[v].eb(1,u);
 	}
-
-	auto ps = centroidDecomposition(TREE);
-	int centroid = find(all(ps), -1) - begin(ps);
-
-	deque<vector<int>> subtrees;
-	ll ansPts = 0;
-	function<int(int ,int)> dfs = [&](int u, int p) {
-		subtrees.back().pb(u);
-		int sz = 1;
-		for (auto v : TREE[u]) {
+	
+	vi dfsOrder;
+	function<void(int ,int)> dfs = [&](int u, int p) {
+		dfsOrder.eb(u);
+		for (int v : ADJ[u]) {
 			if (v != p) {
-				int vsz = dfs(v, u);
-				ansPts += vsz;
-				sz += vsz;
+				dfs(v, u);
 			}
 		}
-		return sz;
 	};
+	auto ps = centroidDecomposition(ADJ);
+	int root = find(all(ps),-1) - begin(ps);
+	dfs(root, -1);
 
-	for(auto &v : TREE[centroid]) {
-		subtrees.eb();
-		ansPts += dfs(v, centroid);
-	}
-
-	sort(all(subtrees), [&](const vector<int>&a, const vector<int>&b) {
-			return len(a) < len(b);
-			});
-
-	ansPts <<= 1ll;
-	
 	vi ansPerm(N);
 	iota(all(ansPerm), 0);
-	dbg(centroid);
-	dbg(subtrees);
-	while (len(subtrees) > 1) {
-		int u = subtrees.back().back();
-		int v = subtrees.front().back();
-
-		swap(ansPerm[u],ansPerm[v]);
-
-		subtrees.back().pop_back();
-		subtrees.front().pop_back();
-
-		if(subtrees.back().empty())
-			subtrees.pop_back();
-		if(subtrees.front().empty())
-			subtrees.pop_front();
-
-
+	rep(i, 0, N) {
+		int j = (i + N/2) % N;
+		ansPerm[dfsOrder[i]] = dfsOrder[j];
 	}
 
-	if (len(subtrees)) 
-		swap(ansPerm[centroid], ansPerm[subtrees.back().back()]);
-	else 
-		swap(ansPerm[centroid], ansPerm[(centroid+1)%N]);
-	
-	cout << ansPts << endl; 
-	for(auto p : ansPerm) cout << p + 1 << ' ';
+	dbg(ansPerm);
+	dbg(dfsOrder);
+	LCA lca(ADJ2, 0);
+	ll ansTot = 0;
+	rep(i, 0, N) {
+		ansTot += lca.dist(i, ansPerm[i]);
+	}
+
+	cout << ansTot << endl;
+	for(int i : ansPerm) cout << i + 1 << ' ';
 	cout << endl;
 }
 
