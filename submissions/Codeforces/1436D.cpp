4165c12d6c7e4cf73496f8ad51a28566ecccaad8
iagorrr <iagorrr04@gmail.com>
2024-10-17 11:52:06 -0300

    trees

---
+++ submissions/Codeforces/1436D.cpp
@@ -0,0 +1,126 @@
+/*8<{==========~ BEGIN TEMPLATE ~============>8*/
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef LOCAL
+#include "debug.cpp"
+#else
+#define dbg(...)
+#endif
+#define fastio                  \
+  ios_base::sync_with_stdio(0); \
+  cin.tie(0);
+#define all(j) j.begin(), j.end()
+#define rall(j) j.rbegin(), j.rend()
+#define len(j) (int)j.size()
+#define rep(i, a, b) \
+  for (common_type_t<decltype(a), decltype(b)> i = (a); i < (b); i++)
+#define rrep(i, a, b) \
+  for (common_type_t<decltype(a), decltype(b)> i = (a); i > (b); i--)
+#define trav(xi, xs) for (auto &xi : xs)
+#define rtrav(xi, xs) for (auto &xi : ranges::views::reverse(xs))
+using ll = long long;
+#define inte ll
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define eb emplace_back
+#define lb lower_bound
+#define ub upper_bound
+#define fi first
+#define se second
+#define emp emplace
+#define ins insert
+#define divc(a, b) ((a) + (b)-1ll) / (b)
+using str = string;
+using ull = unsigned long long;
+using ld = long double;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vll2d = vector<vll>;
+using vi = vector<int>;
+using vi2d = vector<vi>;
+using pii = pair<int, int>;
+using vpii = vector<pii>;
+using vc = vector<char>;
+using vs = vector<str>;
+template <typename T, typename T2>
+using umap = unordered_map<T, T2>;
+template <typename T>
+using pqmn = priority_queue<T, vector<T>, greater<T>>;
+template <typename T>
+using pqmx = priority_queue<T, vector<T>>;
+template <typename T, typename U>
+inline bool chmax(T &a, U const &b) {
+  return (a < b ? a = b, 1 : 0);
+}
+template <typename T, typename U>
+inline bool chmin(T &a, U const &b) {
+  return (a > b ? a = b, 1 : 0);
+}
+/*8<============~ END TEMPLATE ~============}>8*/
+void run();
+
+int32_t main() {
+#ifndef LOCAL
+  fastio;
+#endif
+
+  int T = 1;
+
+  rep(t, 0, T) {
+    dbg(t);
+    run();
+  }
+}
+
+void run() {
+	int N;
+	cin >> N;
+	vi2d ADJ(N);
+	rep(i, 1, N) {
+		int pi;
+		cin >> pi;
+		pi--;
+		ADJ[pi].eb(i);
+	}
+
+	vll AS(N);
+	for (auto &ai: AS) 
+		cin >> ai;
+
+	vll cntLeafs(N), subSum(N), subMx(N);
+	function<void(int)> dfs = [&](int u) {
+		if (len(ADJ[u]) == 0) {
+			cntLeafs[u] = 1;
+			subSum[u] = subMx[u] = AS[u];
+			return;
+		}
+
+		for (int v : ADJ[u]) {
+			dfs(v);
+			chmax(subMx[u], subMx[v]);
+			cntLeafs[u] += cntLeafs[v];
+			subSum[u] += subSum[v];
+		}
+
+		ll toEqualize = cntLeafs[u] * subMx[u] - subSum[u];
+		subSum[u] += AS[u];
+		if(toEqualize < AS[u]) {
+			AS[u] -= toEqualize;
+			subMx[u] = subMx[u] + ((AS[u] +cntLeafs[u]-1)/ cntLeafs[u]);
+		}
+
+
+	};
+
+	dfs(0);
+	dbg(subMx, subSum, cntLeafs);
+
+	cout << subMx[0] << endl;
+
+}
+
+/*8<
+  AC, trees
+>8*/
