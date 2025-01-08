90cc21638ebe638a5ce982282fff28f979732670
iagorrr <iagorrr04@gmail.com>
2025-01-07 21:28:02 -0300

    math, greedy

---
+++ submissions/Codeforces/1343D.cpp
@@ -0,0 +1,117 @@
+/*8<{==========~ BEGIN TEMPLATE ~============>8*/
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef LOCAL
+#include "debug.cpp"
+#else
+#define dbg(...)
+#endif
+#define fastio                        \
+        ios_base::sync_with_stdio(0); \
+        cin.tie(0);
+#define all(j) j.begin(), j.end()
+#define rall(j) j.rbegin(), j.rend()
+#define len(j) (int)j.size()
+#define rep(i, a, b) \
+        for (common_type_t<decltype(a), decltype(b)> i = (a); i < (b); i++)
+#define rrep(i, a, b) \
+        for (common_type_t<decltype(a), decltype(b)> i = (a); i > (b); i--)
+#define trav(xi, xs) for (auto &xi : xs)
+#define rtrav(xi, xs) for (auto &xi : ranges::views::reverse(xs))
+using ll = long long;
+#define endl '\n'
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define eb emplace_back
+#define lb lower_bound
+#define ub upper_bound
+#define fi first
+#define se second
+#define emp emplace
+#define ins insert
+#define divc(a, b) ((a) + (b) - 1ll) / (b)
+using str = string;
+using ull = unsigned long long;
+using ld = long double;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vll2d = vector<vll>;
+using vi = vector<int>;
+using vi2d = vector<vi>;
+using pii = pair<int, int>;
+using vpii = vector<pii>;
+using vc = vector<char>;
+using vs = vector<str>;
+template <typename T, typename T2>
+using umap = unordered_map<T, T2>;
+template <typename T>
+using pqmn = priority_queue<T, vector<T>, greater<T>>;
+template <typename T>
+using pqmx = priority_queue<T, vector<T>>;
+template <typename T, typename U>
+inline bool chmax(T &a, U const &b) {
+        return (a < b ? a = b, 1 : 0);
+}
+template <typename T, typename U>
+inline bool chmin(T &a, U const &b) {
+        return (a > b ? a = b, 1 : 0);
+}
+/*8<============~ END TEMPLATE ~============}>8*/
+void run();
+
+int32_t main() {
+#ifndef LOCAL
+        fastio;
+#endif
+
+        int T = 1;
+
+        cin >> T;
+
+        rep(t, 0, T) {
+                dbg(t);
+                run();
+        }
+}
+
+void run() {
+        int N, K;
+        cin >> N >> K;
+        vi AS(N);
+        trav(ai, AS) { cin >> ai; }
+
+        vi2d closes(K * 2 + 1);
+        vi hist(2 * K + 1);
+        for (int l = 0, r = N - 1; l < r; l++, r--) {
+                int al = AS[l], ar = AS[r];
+                if (al > ar) swap(al, ar);
+
+                hist[al + ar]++;
+
+                int smn = al + 1;
+                int smx = ar + K;
+
+                closes[smn].eb(smx);
+        }
+
+        int ans = INT_MAX;
+
+        pqmn<int> pq;
+        rep(i, 1, 2 * K + 1) {
+                for (auto v : closes[i]) pq.emp(v);
+                while (len(pq) and pq.top() < i) {
+                        pq.pop();
+                }
+
+                int cost = 2 * (N / 2 - len(pq)) + len(pq) - hist[i];
+                dbg(i, cost);
+                chmin(ans, cost);
+        }
+
+        cout << ans << endl;
+}
+
+/*8<
+>8*/
