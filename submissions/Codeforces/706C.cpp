709c5bafa6d88808c69b9685703fed71d154113a
iagorrr <iagorrr04@gmail.com>
2025-02-02 22:21:29 -0300

    dynamic programming, strings

---
+++ submissions/Codeforces/706C.cpp
@@ -0,0 +1,132 @@
+/*8<{==========~ BEGIN TEMPLATE ~============>8*/
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef LOCAL
+#include "debug.cpp"
+#else
+#define dbg(...)
+#endif
+#define fastio                        \
+        ios_base::sync_with_stdio(0); \
+        cin.tie(0);
+#define all(j) j.begin(), j.end()
+#define rall(j) j.rbegin(), j.rend()
+#define len(j) (int)j.size()
+#define rep(i, a, b) \
+        for (common_type_t<decltype(a), decltype(b)> i = (a); i < (b); i++)
+#define rrep(i, a, b) \
+        for (common_type_t<decltype(a), decltype(b)> i = (a); i > (b); i--)
+#define trav(xi, xs) for (auto &xi : xs)
+#define rtrav(xi, xs) for (auto &xi : ranges::views::reverse(xs))
+using ll = long long;
+#define endl '\n'
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define eb emplace_back
+#define lb lower_bound
+#define ub upper_bound
+#define fi first
+#define se second
+#define emp emplace
+#define ins insert
+#define divc(a, b) ((a) + (b) - 1ll) / (b)
+using str = string;
+using ull = unsigned long long;
+using ld = long double;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vll2d = vector<vll>;
+using vi = vector<int>;
+using vi2d = vector<vi>;
+using pii = pair<int, int>;
+using vpii = vector<pii>;
+using vc = vector<char>;
+using vs = vector<str>;
+template <typename T, typename T2>
+using umap = unordered_map<T, T2>;
+template <typename T>
+using pqmn = priority_queue<T, vector<T>, greater<T>>;
+template <typename T>
+using pqmx = priority_queue<T, vector<T>>;
+template <typename T, typename U>
+inline bool chmax(T &a, U const &b) {
+        return (a < b ? a = b, 1 : 0);
+}
+template <typename T, typename U>
+inline bool chmin(T &a, U const &b) {
+        return (a > b ? a = b, 1 : 0);
+}
+
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &vec) {
+        for (auto &element : vec) {
+                is >> element;
+        }
+        return is;
+}
+
+/*8<============~ END TEMPLATE ~============}>8*/
+void run();
+
+int32_t main() {
+#ifndef LOCAL
+        fastio;
+#endif
+
+        int T = 1;
+
+        /*cin >> T;*/
+
+        rep(t, 0, T) {
+                dbg(t);
+                run();
+        }
+}
+
+const int MAXN = 1'00'000 + 1;
+ll memo[MAXN][2];
+void run() {
+        memset(memo, -1, sizeof memo);
+
+        int N;
+        cin >> N;
+
+        vll CS(N);
+        cin >> CS;
+
+        vs S(N);
+        cin >> S;
+
+        vs RS(all(S));
+        for_each(all(RS), [](auto &x) { reverse(all(x)); });
+        dbg(S, RS);
+
+        const ll oo = 1e14;
+
+        function<ll(int, int)> solve = [&](int i, int p) {
+                if (i >= N) return 0ll;
+
+                auto &ret = memo[i][p];
+                if (~ret) return ret;
+
+                ll ans = oo;
+
+                if (i) {
+                        auto &ps = p ? RS[i - 1] : S[i - 1];
+                        if (S[i] >= ps) chmin(ans, solve(i + 1, 0));
+                        if (RS[i] >= ps) chmin(ans, CS[i] + solve(i + 1, 1));
+                } else
+                        ans = min({solve(i + 1, 0), CS[i] + solve(i + 1, 1)});
+
+                return ret = ans;
+        };
+
+        auto ans = solve(0, 0);
+        cout << (ans >= oo ? -1 : ans) << endl;
+}
+
+/*8<
+ * AC, dynamic programming, strings
+>8*/
