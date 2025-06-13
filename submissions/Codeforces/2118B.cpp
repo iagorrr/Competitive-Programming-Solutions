77cd9063d55b1efb0fcc891bcc6a903b588e812d
iagorrr <iagorrr04@gmail.com>
2025-06-13 13:37:39 -0300

    greedy, constructive algorithms

---
+++ submissions/Codeforces/2118B.cpp
@@ -0,0 +1,149 @@
+/*8<{==========~ BEGIN TEMPLATE ~============>8*/
+#include <bits/stdc++.h>
+
+using namespace std;
+#ifdef LOCAL
+#include "debug.cpp"
+#else
+#define dbg(...)
+#endif
+#define fastio                        \
+        ios_base::sync_with_stdio(0); \
+        cin.tie(0);
+#define all(j) j.begin(), j.end()
+#define rall(j) j.rbegin(), j.rend()
+#define len(j) (int)j.size()
+#define rep(i, a, b) \
+        for (common_type_t<decltype(a), decltype(b)> i = (a); i < (b); i++)
+#define rrep(i, a, b) \
+        for (common_type_t<decltype(a), decltype(b)> i = (a); i > (b); i--)
+#define trav(xi, xs) for (auto &xi : xs)
+#define rtrav(xi, xs) for (auto &xi : ranges::views::reverse(xs))
+using ll = long long;
+#define endl '\n'
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define eb emplace_back
+#define ef emplace_back
+#define lb lower_bound
+#define ub upper_bound
+#define fi first
+#define se second
+#define emp emplace
+#define ins insert
+#define divc(a, b) ((a) + (b) - 1ll) / (b)
+using str = string;
+using ull = unsigned long long;
+using ld = long double;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vll2d = vector<vll>;
+using vi = vector<int>;
+using vi2d = vector<vi>;
+using pii = pair<int, int>;
+using vpii = vector<pii>;
+using vc = vector<char>;
+using vs = vector<str>;
+template <typename T, typename T2>
+using umap = unordered_map<T, T2>;
+template <typename T>
+using pqmn = priority_queue<T, vector<T>, greater<T>>;
+template <typename T>
+using pqmx = priority_queue<T, vector<T>>;
+template <typename T, typename U>
+inline bool chmax(T &a, U const &b) {
+        return (a < b ? a = b, 1 : 0);
+}
+template <typename T, typename U>
+inline bool chmin(T &a, U const &b) {
+        return (a > b ? a = b, 1 : 0);
+}
+
+// read vector
+// TODO: abstract this to any container.
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &vec) {
+        for (auto &element : vec) {
+                is >> element;
+        }
+        return is;
+}
+
+// print vector
+// TODO: abstract this to any container.
+template <typename T>
+ostream &operator<<(ostream &os, vector<T> &xs) {
+        rep(i, os.iword(0), xs.size()) os << xs[i]
+                                          << (i == xs.size() ? "" : " ");
+        os.iword(0) = 0;
+        return os;
+}
+
+// sum a vector, using the default constructor as initial value
+// TODO: abstract this to any container.
+template <typename T>
+T sumList(vector<T> &xs) {
+        return accumulate(all(xs), T());
+}
+
+/*8<============~ END TEMPLATE ~============}>8*/
+void __init();
+void __run();
+void __pre_run();
+
+int32_t main() {
+#ifndef LOCAL
+        fastio;
+#endif
+        __init();
+
+        int T = 1;
+
+        cin >> T;
+
+        rep(t, 0, T) {
+                dbg(t);
+                __pre_run();
+                __run();
+        }
+}
+
+/*
+ *
+ *
+ *
+ *
+ *
+ * */
+
+void __init() {}
+
+void __pre_run() {}
+
+void __run() {
+        int n;
+        cin >> n;
+
+        vector<tuple<int, int, int>> ans;
+
+        int j = 1;
+        rep(i, 0, n - 1) {
+                ans.eb(i, 0, j);
+                ans.eb(i, j + 1, n - 1);
+                j++;
+        }
+        ans.ppb();
+        ans.eb(n - 1, 1, n - 1);
+
+        cout << len(ans) << endl;
+
+        for (auto [i, l, r] : ans) {
+                cout << i + 1 << ' ' << l + 1 << ' ' << r + 1 << endl;
+        }
+}
+
+/*8<
+ * AC, constructive algorithms
+>8*/
