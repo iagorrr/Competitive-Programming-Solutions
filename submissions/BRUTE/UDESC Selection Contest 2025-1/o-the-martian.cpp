102bfc35849e3e43b85702cfabaa1fdea1874661
iagorrr <iagorrr04@gmail.com>
2025-06-17 08:53:18 -0300

    /*8<{==========~ BEGIN TEMPLATE ~============>8*/ using namespace std;         ios_base::sync_with_stdio(0); \         cin.tie(0);         for (common_type_t<decltype(a), decltype(b)> i = (a); i < (b); i++)         for (common_type_t<decltype(a), decltype(b)> i = (a); i > (b); i--) using ll = long long; using str = string; using ull = unsigned long long; using ld = long double; using vll = vector<ll>; using pll = pair<ll, ll>; using vll2d = vector<vll>; using vi = vector<int>; using vi2d = vector<vi>; using pii = pair<int, int>; using vpii = vector<pii>; using vc = vector<char>; using vs = vector<str>;
    TTU using umap = unordered_map<T, U>;
    TT using pqmn = priority_queue<T, vector<T>, greater<T>>;
    TT using pqmx = priority_queue<T, vector<T>>;
    
    TTU inline bool chmax(T &a, U const &b) { return (a < b ? a = b, 1 : 0); }
    TTU inline bool chmin(T &a, U const &b) { return (a > b ? a = b, 1 : 0); }
    
    // read vector
    // TODO: abstract this to any container.
    TT std::istream &operator>>(std::istream &is, std::vector<T> &vec) {
            for (auto &element : vec) {
                    is >> element;
            }
            return is;
    }
    
    // print vector
    // TODO: abstract this to any container.
    TT ostream &operator<<(ostream &os, vector<T> &xs) {
            rep(i, os.iword(0), xs.size()) os << xs[i]
                                              << (i == xs.size() ? "" : " ");
            os.iword(0) = 0;
            return os;
    }
    
    // sum a vector, using the default constructor as initial value
    // TODO: abstract this to any container.
    TT T sumList(vector<T> &xs) { return accumulate(all(xs), T()); }
    
    /*
     * verify if a `predicate` is valid for
     * values in range [l, r]
     *
     * TODO: put a type in predicate ??
     * */
    // bool all_of_in_range(T l, T r, auto predicate) {
    //     return ranges::all_of(views::iota(l, r + 1),
    //                           [&](T v) { return predicate(v); });
    // }
    
    // checks if a  value v is the interval [l, r]
    TT bool inline between(T v, T l, T r) { return clamp(v, l, r) == v; }
    
    /*8<============~ END TEMPLATE ~============}>8*/
    void __init();
    void __run();
    void __pre_run();
    
    int32_t main() {
            fastio;
            __init();
    
            int T = 1;
    
            // cin >> T;
    
            rep(t, 0, T) {
                    dbg(t);
                    __pre_run();
                    __run();
            }
    }
    
    /*
     *
     *
     *
     *
     *
     * */
    
    void __init() {}
    
    void __pre_run() {}
    
    vi ssase(vector<vector<pii>> &g) {
            int n = len(g);
    
            vi ds(n, INT_MAX);
            pqmn<pii> pq;
    
            ds[0] = 0;
            pq.emp(0, 0);
    
            while (len(pq)) {
                    auto [eu, u] = pq.top();
                    pq.pop();
    
                    if (ds[u] < eu) continue;
    
                    for (auto [w, v] : g[u]) {
                            int w2 = max(w, eu);
                            if (w2 < ds[v]) {
                                    ds[v] = w2;
                                    pq.emp(w2, v);
                            }
                    }
            }
    
            return ds;
    }
    
    void __run() {
            int n, m;
            cin >> n >> m;
    
            vector<vector<pii>> g(n);
            rep(i, 0, m) {
                    int u, v, w;
                    cin >> u >> v >> w;
                    u--, v--;
                    g[u].eb(w, v);
                    g[v].eb(w, u);
            }
    
            int d;
            cin >> d;
    
            vi ds(d);
            cin >> ds;
    
            vi es(d);
            cin >> es;
    
            dbg(es);
    
            vi ms = ssase(g);
            dbg(ms);
    
            vi ns;
            trav(di, ds) ns.eb(ms[di - 1]);
    
            sort(all(ns));
            sort(all(es));
    
            dbg(ns);
            dbg(es);
    
            int ans = 0;
    
            while (len(ns) and len(es)) {
                    if (es.back() >= ns.back()) {
                            es.ppb();
                            ans++;
                    }
    
                    ns.ppb();
            }
    
            cout << ans << endl;
    }
    
    /*8<
    >8*/
    shortest paths

---
+++ submissions/BRUTE/UDESC Selection Contest 2025-1/o-the-martian.cpp
@@ -0,0 +1,208 @@
+/*8<{==========~ BEGIN TEMPLATE ~============>8*/
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef LOCAL
+#include "debug.cpp"
+#else
+#define dbg(...)
+#endif
+#define fastio                        \
+        ios_base::sync_with_stdio(0); \
+        cin.tie(0);
+#define all(j) j.begin(), j.end()
+#define rall(j) j.rbegin(), j.rend()
+#define len(j) (int)j.size()
+#define rep(i, a, b) \
+        for (common_type_t<decltype(a), decltype(b)> i = (a); i < (b); i++)
+#define rrep(i, a, b) \
+        for (common_type_t<decltype(a), decltype(b)> i = (a); i > (b); i--)
+#define trav(xi, xs) for (auto &xi : xs)
+#define rtrav(xi, xs) for (auto &xi : ranges::views::reverse(xs))
+using ll = long long;
+#define endl '\n'
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define eb emplace_back
+#define ef emplace_back
+#define lb lower_bound
+#define ub upper_bound
+#define fi first
+#define se second
+#define emp emplace
+#define ins insert
+#define divc(a, b) ((a) + (b) - 1ll) / (b)
+using str = string;
+using ull = unsigned long long;
+using ld = long double;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vll2d = vector<vll>;
+using vi = vector<int>;
+using vi2d = vector<vi>;
+using pii = pair<int, int>;
+using vpii = vector<pii>;
+using vc = vector<char>;
+using vs = vector<str>;
+
+#define TT template <typename T>
+#define TTU template <typename T, typename U>
+
+TTU using umap = unordered_map<T, U>;
+TT using pqmn = priority_queue<T, vector<T>, greater<T>>;
+TT using pqmx = priority_queue<T, vector<T>>;
+
+TTU inline bool chmax(T &a, U const &b) { return (a < b ? a = b, 1 : 0); }
+TTU inline bool chmin(T &a, U const &b) { return (a > b ? a = b, 1 : 0); }
+
+// read vector
+// TODO: abstract this to any container.
+TT std::istream &operator>>(std::istream &is, std::vector<T> &vec) {
+        for (auto &element : vec) {
+                is >> element;
+        }
+        return is;
+}
+
+// print vector
+// TODO: abstract this to any container.
+TT ostream &operator<<(ostream &os, vector<T> &xs) {
+        rep(i, os.iword(0), xs.size()) os << xs[i]
+                                          << (i == xs.size() ? "" : " ");
+        os.iword(0) = 0;
+        return os;
+}
+
+// sum a vector, using the default constructor as initial value
+// TODO: abstract this to any container.
+TT T sumList(vector<T> &xs) { return accumulate(all(xs), T()); }
+
+/*
+ * verify if a `predicate` is valid for
+ * values in range [l, r]
+ *
+ * TODO: put a type in predicate ??
+ * */
+// bool all_of_in_range(T l, T r, auto predicate) {
+//     return ranges::all_of(views::iota(l, r + 1),
+//                           [&](T v) { return predicate(v); });
+// }
+
+// checks if a  value v is the interval [l, r]
+TT bool inline between(T v, T l, T r) { return clamp(v, l, r) == v; }
+
+/*8<============~ END TEMPLATE ~============}>8*/
+void __init();
+void __run();
+void __pre_run();
+
+int32_t main() {
+#ifndef LOCAL
+        fastio;
+#endif
+        __init();
+
+        int T = 1;
+
+        // cin >> T;
+
+        rep(t, 0, T) {
+                dbg(t);
+                __pre_run();
+                __run();
+        }
+}
+
+/*
+ *
+ *
+ *
+ *
+ *
+ * */
+
+void __init() {}
+
+void __pre_run() {}
+
+vi ssase(vector<vector<pii>> &g) {
+        int n = len(g);
+
+        vi ds(n, INT_MAX);
+        pqmn<pii> pq;
+
+        ds[0] = 0;
+        pq.emp(0, 0);
+
+        while (len(pq)) {
+                auto [eu, u] = pq.top();
+                pq.pop();
+
+                if (ds[u] < eu) continue;
+
+                for (auto [w, v] : g[u]) {
+                        int w2 = max(w, eu);
+                        if (w2 < ds[v]) {
+                                ds[v] = w2;
+                                pq.emp(w2, v);
+                        }
+                }
+        }
+
+        return ds;
+}
+
+void __run() {
+        int n, m;
+        cin >> n >> m;
+
+        vector<vector<pii>> g(n);
+        rep(i, 0, m) {
+                int u, v, w;
+                cin >> u >> v >> w;
+                u--, v--;
+                g[u].eb(w, v);
+                g[v].eb(w, u);
+        }
+
+        int d;
+        cin >> d;
+
+        vi ds(d);
+        cin >> ds;
+
+        vi es(d);
+        cin >> es;
+
+        dbg(es);
+
+        vi ms = ssase(g);
+        dbg(ms);
+
+        vi ns;
+        trav(di, ds) ns.eb(ms[di - 1]);
+
+        sort(all(ns));
+        sort(all(es));
+
+        dbg(ns);
+        dbg(es);
+
+        int ans = 0;
+
+        while (len(ns) and len(es)) {
+                if (es.back() >= ns.back()) {
+                        es.ppb();
+                        ans++;
+                }
+
+                ns.ppb();
+        }
+
+        cout << ans << endl;
+}
+
+/*8<
+ * AC, shortest paths.
+>8*/
