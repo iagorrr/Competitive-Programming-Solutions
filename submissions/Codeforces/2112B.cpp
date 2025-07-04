e8d70dd238691006a1acbde06ea743227ec373f8
iagorrr <iagorrr04@gmail.com>
2025-07-04 11:09:01 -0300

    adds Codeforces 2112 A-C

---
+++ submissions/Codeforces/2112B.cpp
@@ -0,0 +1,163 @@
+#pragma once
+/*8<{==========~ BEGIN TEMPLATE ~============>8*/
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef LOCAL
+#include "debug.cpp"
+#else
+#define dbg(...)
+#endif
+#define fastio                        \
+        ios_base::sync_with_stdio(0); \
+        cin.tie(0);
+#define all(j) j.begin(), j.end()
+#define rall(j) j.rbegin(), j.rend()
+#define len(j) (int)j.size()
+#define rep(i, a, b) \
+        for (common_type_t<decltype(a), decltype(b)> i = (a); i < (b); i++)
+#define rrep(i, a, b) \
+        for (common_type_t<decltype(a), decltype(b)> i = (a); i > (b); i--)
+#define trav(xi, xs) for (auto &xi : xs)
+#define rtrav(xi, xs) for (auto &xi : ranges::views::reverse(xs))
+#define loop while (1)
+using ll = long long;
+#define endl '\n'
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define eb emplace_back
+#define ef emplace_back
+#define lb lower_bound
+#define ub upper_bound
+#define fi first
+#define se second
+#define emp emplace
+#define ins insert
+#define divc(a, b) ((a) + (b) - 1ll) / (b)
+using str = string;
+using ull = unsigned long long;
+using ld = long double;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vll2d = vector<vll>;
+using vi = vector<int>;
+using vi2d = vector<vi>;
+using pii = pair<int, int>;
+using vpii = vector<pii>;
+using vc = vector<char>;
+using vs = vector<str>;
+
+#define TT template <typename T>
+#define TTU template <typename T, typename U>
+
+TTU using umap = unordered_map<T, U>;
+TT using pqmn = priority_queue<T, vector<T>, greater<T>>;
+TT using pqmx = priority_queue<T, vector<T>>;
+
+TTU inline bool chmax(T &a, U const &b) { return (a < b ? a = b, 1 : 0); }
+TTU inline bool chmin(T &a, U const &b) { return (a > b ? a = b, 1 : 0); }
+
+// read vector
+// TODO: abstract this to any container.
+TT std::istream &operator>>(std::istream &is, std::vector<T> &vec) {
+        for (auto &element : vec) {
+                is >> element;
+        }
+        return is;
+}
+
+// print vector
+// TODO: abstract this to any container.
+TT ostream &operator<<(ostream &os, vector<T> &xs) {
+        rep(i, os.iword(0), xs.size()) os << xs[i]
+                                          << (i == xs.size() ? "" : " ");
+        os.iword(0) = 0;
+        return os;
+}
+
+// sum a vector, using the default constructor as initial value
+// TODO: abstract this to any container.
+TT T sumList(vector<T> &xs) { return accumulate(all(xs), T()); }
+
+/*
+ * verify if a `predicate` is valid for
+ * values in range [l, r]
+ *
+ * TODO: put a type in predicate ??
+ * */
+// bool all_of_in_range(T l, T r, auto predicate) {
+//     return ranges::all_of(views::iota(l, r + 1),
+//                           [&](T v) { return predicate(v); });
+// }
+
+// checks if a  value v is the interval [l, r]
+TT bool inline between(T v, T l, T r) { return clamp(v, l, r) == v; }
+
+/*8<============~ END TEMPLATE ~============}>8*/
+void __init();
+void __run();
+void __pre_run();
+
+int32_t main() {
+#ifndef LOCAL
+        fastio;
+#endif
+        __init();
+
+        int T = 1;
+
+        cin >> T;
+
+        rep(t, 0, T) {
+                dbg(t);
+                __pre_run();
+                __run();
+        }
+}
+
+/*
+ *
+ *
+ *
+ *
+ *
+ * */
+
+void __init() {}
+
+void __pre_run() {}
+
+void __run() {
+        int n;
+        cin >> n;
+
+        vi as(n + 2);
+        rep(i, 1, n + 1) cin >> as[i];
+        as.back() = as.front() = 1e9;
+
+        int ans = INT_MAX;
+
+        rep(l, 1ll, n + 1) {
+                int mn = as[l];
+                int mx = as[l];
+                rep(r, l, n + 1) {
+                        chmin(mn, as[r]);
+                        chmax(mx, as[r]);
+
+                        for (auto v : {as[l - 1], as[r + 1]}) {
+                                rep(v2, v - 1, v + 2) {
+                                        if (clamp(v2, mn, mx) == v2) {
+                                                dbg(l, r);
+                                                chmin(ans, r - l);
+                                        }
+                                }
+                        }
+                }
+        }
+
+        cout << (ans == INT_MAX ? -1 : ans) << endl;
+}
+
+/*8<
+>8*/
---
+++ submissions/Codeforces/2112C.cpp
@@ -0,0 +1,197 @@
+/*8<{==========~ BEGIN TEMPLATE ~============>8*/
+#include <bits/stdc++.h>
+
+#include <algorithm>
+using namespace std;
+#ifdef LOCAL
+#include "debug.cpp"
+#else
+#define dbg(...)
+#endif
+#define fastio                        \
+        ios_base::sync_with_stdio(0); \
+        cin.tie(0);
+#define all(j) j.begin(), j.end()
+#define rall(j) j.rbegin(), j.rend()
+#define len(j) (int)j.size()
+#define rep(i, a, b) \
+        for (common_type_t<decltype(a), decltype(b)> i = (a); i < (b); i++)
+#define rrep(i, a, b) \
+        for (common_type_t<decltype(a), decltype(b)> i = (a); i > (b); i--)
+#define trav(xi, xs) for (auto &xi : xs)
+#define rtrav(xi, xs) for (auto &xi : ranges::views::reverse(xs))
+#define loop while (1)
+using ll = long long;
+#define endl '\n'
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define eb emplace_back
+#define ef emplace_back
+#define lb lower_bound
+#define ub upper_bound
+#define fi first
+#define se second
+#define emp emplace
+#define ins insert
+#define divc(a, b) ((a) + (b) - 1ll) / (b)
+using str = string;
+using ull = unsigned long long;
+using ld = long double;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vll2d = vector<vll>;
+using vi = vector<int>;
+using vi2d = vector<vi>;
+using pii = pair<int, int>;
+using vpii = vector<pii>;
+using vc = vector<char>;
+using vs = vector<str>;
+
+#define TT template <typename T>
+#define TTU template <typename T, typename U>
+
+TTU using umap = unordered_map<T, U>;
+TT using pqmn = priority_queue<T, vector<T>, greater<T>>;
+TT using pqmx = priority_queue<T, vector<T>>;
+
+TTU inline bool chmax(T &a, U const &b) { return (a < b ? a = b, 1 : 0); }
+TTU inline bool chmin(T &a, U const &b) { return (a > b ? a = b, 1 : 0); }
+
+// read vector
+// TODO: abstract this to any container.
+TT std::istream &operator>>(std::istream &is, std::vector<T> &vec) {
+        for (auto &element : vec) {
+                is >> element;
+        }
+        return is;
+}
+
+// print vector
+// TODO: abstract this to any container.
+TT ostream &operator<<(ostream &os, vector<T> &xs) {
+        rep(i, os.iword(0), xs.size()) os << xs[i]
+                                          << (i == xs.size() ? "" : " ");
+        os.iword(0) = 0;
+        return os;
+}
+
+// sum a vector, using the default constructor as initial value
+// TODO: abstract this to any container.
+TT T sumList(vector<T> &xs) { return accumulate(all(xs), T()); }
+
+/*
+ * verify if a `predicate` is valid for
+ * values in range [l, r]
+ *
+ * TODO: put a type in predicate ??
+ * */
+// bool all_of_in_range(T l, T r, auto predicate) {
+//     return ranges::all_of(views::iota(l, r + 1),
+//                           [&](T v) { return predicate(v); });
+// }
+
+// checks if a  value v is the interval [l, r]
+TT bool inline between(T v, T l, T r) { return clamp(v, l, r) == v; }
+
+/*8<============~ END TEMPLATE ~============}>8*/
+void __init();
+void __run();
+void __pre_run();
+
+int32_t main() {
+#ifndef LOCAL
+        fastio;
+#endif
+        __init();
+
+        int T = 1;
+
+        cin >> T;
+
+        rep(t, 0, T) {
+                dbg(t);
+                __pre_run();
+                __run();
+        }
+}
+
+void __init() {}
+
+void __pre_run() {}
+/*8<
+@Title: Max \& Min Check
+
+@Description: Returns the min/max value in range
+[l, r] that satisfies the lambda function check,
+if there is no such value the 'nullopt' is
+returned.
+
+@Usage: check must be a function that receives
+an integer and return a boolean.
+
+@Time: $O(\log{r-l+1})$
+>8*/
+
+template <typename T>
+optional<T> maxCheck(T l, T r, auto check) {
+        optional<T> ret;
+        while (l <= r) {
+                T m = midpoint(l, r);
+                if (check(m))
+                        ret ? chmax(ret, m) : ret = m, l = m + 1;
+                else
+                        r = m - 1;
+        }
+        return ret;
+}
+
+template <typename T>
+optional<T> minCheck(T l, T r, auto check) {
+        optional<T> ret;
+        while (l <= r) {
+                T m = midpoint(l, r);
+                if (check(m))
+                        ret ? chmin(ret, m) : ret = m, r = m - 1;
+                else
+                        l = m + 1;
+        }
+        return ret;
+}
+
+void __run() {
+        int n;
+        cin >> n;
+
+        vll as(n);
+        cin >> as;
+
+        sort(all(as));
+
+        ll ans = 0;
+
+        rep(l, 0, n) {
+                rep(r, l + 2, n) {
+                        auto m = minCheck(l + 1, r - 1, [&](int x) {
+                                return as[l] + as[r] -
+                                               max(as[r] * 2, as.back()) +
+                                               as[x] >
+                                       0;
+                        });
+
+                        if (m) ans += (r - *m);
+                }
+        }
+
+        cout << ans << endl;
+}
+
+/*8<
+* bob always take the greatest element
+* corner case when alice also choose the largest element and there is only one
+*
+* some linear approach with a binary search to find the range
+*
+*
+copy. >8*/
