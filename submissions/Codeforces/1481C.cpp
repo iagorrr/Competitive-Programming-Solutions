a5f0bb76477c040b1f44d70c6587cd7e2f387d81
iago.rocha <iagorrr04@gmail.com>
2025-09-16 13:24:51 -0300

    greedy

---
+++ submissions/Codeforces/1481C.cpp
@@ -0,0 +1,199 @@
+/*8<{==========~ BEGIN TEMPLATE ~============>8*/
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef LOCAL
+#include "debug.cpp"
+#else
+#define dbg(...)
+#endif
+#define fastio                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);
+#define all(j) j.begin(), j.end()
+#define rall(j) j.rbegin(), j.rend()
+#define len(j) (int)j.size()
+#define rep(i, a, b) \
+    for (common_type_t<decltype(a), decltype(b)> i = (a); i < (b); i++)
+#define rrep(i, a, b) \
+    for (common_type_t<decltype(a), decltype(b)> i = (a); i > (b); i--)
+#define repn(i, b) for (auto i = 0; i < (b); i++)
+#define trav(xi, xs) for (auto &xi : xs)
+#define rtrav(xi, xs) for (auto &xi : ranges::views::reverse(xs))
+#define loop while (1)
+using ll = long long;
+#define endl '\n'
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define eb emplace_back
+#define ef emplace_back
+#define lb lower_bound
+#define ub upper_bound
+#define fi first
+#define se second
+#define emp emplace
+#define ins insert
+#define divc(a, b) ((a) + (b) - 1ll) / (b)
+#define NO cout << "NO\n"
+#define YES cout << "YES\n"
+using str = string;
+using ull = unsigned long long;
+using ld = long double;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vll2d = vector<vll>;
+using vi = vector<int>;
+using vi2d = vector<vi>;
+using pii = pair<int, int>;
+using vpii = vector<pii>;
+using vc = vector<char>;
+using vs = vector<str>;
+
+#define TT template <typename T>
+#define TTU template <typename T, typename U>
+
+TTU using umap = unordered_map<T, U>;
+TT using pqmn = priority_queue<T, vector<T>, greater<T>>;
+TT using pqmx = priority_queue<T, vector<T>>;
+
+TTU inline bool chmax(T &a, U const &b) { return (a < b ? a = b, 1 : 0); }
+TTU inline bool chmin(T &a, U const &b) { return (a > b ? a = b, 1 : 0); }
+
+// read vector
+// TODO: abstract this to any container.
+TT std::istream &operator>>(std::istream &is, std::vector<T> &vec) {
+    for (auto &element : vec) {
+        is >> element;
+    }
+    return is;
+}
+
+// print vector
+// TODO: abstract this to any container.
+TT ostream &operator<<(ostream &os, vector<T> &xs) {
+    rep(i, os.iword(0), xs.size()) os << xs[i] << (i == xs.size() ? "" : " ");
+    os.iword(0) = 0;
+    return os;
+}
+
+// print multiset
+// TODO: abstract this to any container.
+TT ostream &operator<<(ostream &os, multiset<T> &xs) {
+    for (auto it = xs.begin(); it != xs.end(); it++)
+        os << *it << (next(it) == end(xs) ? "" : " ");
+    return os;
+}
+
+// sum a vector, using the default constructor as initial value
+// TODO: abstract this to any container.
+TT T sumList(vector<T> &xs) { return accumulate(all(xs), T()); }
+
+/*
+ * verify if a `predicate` is valid for
+ * values in range [l, r]
+ *
+ * TODO: put a type in predicate ??
+ * */
+// bool all_of_in_range(T l, T r, auto predicate) {
+//     return ranges::all_of(views::iota(l, r + 1),
+//                           [&](T v) { return predicate(v); });
+// }
+
+// checks if a  value v is the interval [l, r]
+TT bool inline between(T v, T l, T r) { return clamp(v, l, r) == v; }
+
+/*8<============~ END TEMPLATE ~============}>8*/
+void __init();
+void __run();
+void __pre_run();
+
+int32_t main() {
+#ifndef LOCAL
+    fastio;
+#endif
+    __init();
+
+    int T = 1;
+
+    cin >> T;
+
+    rep(t, 0, T) {
+        dbg(t);
+        __pre_run();
+        __run();
+    }
+}
+
+/*
+ *
+ *
+ *
+ *
+ *
+ * */
+
+void __init() {}
+
+void __pre_run() {}
+
+void __run() {
+	int n, m; cin >> n >> m;
+	vi as(n); cin >> as;
+	vi bs(n); cin >> bs;
+	vi cs(m); cin >> cs;
+
+
+
+	int p = -1;
+	repn(i,n){
+		if(bs[i]==cs.back()) p = i;
+	}
+	repn(i,n){
+		if(bs[i]==cs.back() and as[i]!=bs[i]) p = i;
+	}
+
+
+	if (p == -1) {
+		NO;
+		return;
+	}
+
+	vector<vector<int>> ws(n+1);
+
+	repn(i,n) {
+		if (i != p and as[i] != bs[i])
+			ws[bs[i]].pb(i);
+	}
+
+
+	vi ans;
+	cs.pop_back();
+
+	trav(ci, cs) {
+		if (len(ws[ci])) {
+			int i = ws[ci].back(); ws[ci].ppb();
+			ans.pb(i+1);
+			as[i]=ci;
+		}
+		else {
+			ans.pb(p+1);
+			as[p]=ci;
+		}
+	}
+
+	as[p]=bs[p];
+	ans.push_back(p+1);
+
+	if (as != bs) {
+		NO;
+		return;
+	}
+
+	YES;
+	cout << ans << endl;
+}
+
+/*8<
+ * AC, greedy
+>8*/
