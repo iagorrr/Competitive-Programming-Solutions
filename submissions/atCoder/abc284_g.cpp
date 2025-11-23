762b5c718e64ad6d8d4c8d9c982f7b047996e9cd
iagorrr <iagorrr04@gmail.com>
2025-11-23 13:53:01 -0300

    math, combinatorics, graph

---
+++ submissions/atCoder/abc284_g.cpp
@@ -0,0 +1,215 @@
+/*
+
+
+
+*/
+/*8<{==========~ BEGIN TEMPLATE ~============>8*/
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef LOCAL
+#include "debug.cpp"
+#else
+#define dbg(...)
+#endif
+#define fastio                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);
+#define all(j) j.begin(), j.end()
+#define rall(j) j.rbegin(), j.rend()
+#define len(j) (int)j.size()
+#define rep(i, a, b) \
+    for (common_type_t<decltype(a), decltype(b)> i = (a); i < (b); i++)
+#define rrep(i, a, b) \
+    for (common_type_t<decltype(a), decltype(b)> i = (a); i > (b); i--)
+#define repn(i, b) for (auto i = 0; i < (b); i++)
+#define trav(xi, xs) for (auto &xi : xs)
+#define rtrav(xi, xs) for (auto &xi : ranges::views::reverse(xs))
+#define loop while (1)
+using ll = long long;
+#define endl '\n'
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define eb emplace_back
+#define ef emplace_back
+#define lb lower_bound
+#define ub upper_bound
+#define fi first
+#define se second
+#define emp emplace
+#define ins insert
+#define divc(a, b) ((a) + (b) - 1ll) / (b)
+using str = string;
+using ull = unsigned long long;
+using ld = long double;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vll2d = vector<vll>;
+using vi = vector<int>;
+using vi2d = vector<vi>;
+using pii = pair<int, int>;
+using vpii = vector<pii>;
+using vc = vector<char>;
+using vs = vector<str>;
+
+#define TT template <typename T>
+#define TTU template <typename T, typename U>
+
+TTU using umap = unordered_map<T, U>;
+TT using pqmn = priority_queue<T, vector<T>, greater<T>>;
+TT using pqmx = priority_queue<T, vector<T>>;
+
+TTU inline bool chmax(T &a, U const &b) { return (a < b ? a = b, 1 : 0); }
+TTU inline bool chmin(T &a, U const &b) { return (a > b ? a = b, 1 : 0); }
+
+// read pair :)
+TTU std::istream &operator>>(std::istream &is, std::pair<T, U> &p) {
+    is >> p.first >> p.second;
+    return is;
+}
+
+// read vector
+// TODO: abstract this to any container.
+TT std::istream &operator>>(std::istream &is, std::vector<T> &vec) {
+    for (auto &element : vec) {
+        is >> element;
+    }
+    return is;
+}
+
+// print vector
+// TODO: abstract this to any container.
+TT ostream &operator<<(ostream &os, vector<T> &xs) {
+    rep(i, os.iword(0), xs.size()) os << xs[i] << (i == xs.size() ? "" : " ");
+    os.iword(0) = 0;
+    return os;
+}
+
+// print multiset
+// TODO: abstract this to any container.
+TT ostream &operator<<(ostream &os, multiset<T> &xs) {
+    for (auto it = xs.begin(); it != xs.end(); it++)
+        os << *it << (next(it) == end(xs) ? "" : " ");
+    return os;
+}
+
+// sum a vector, using the default constructor as initial value
+// TODO: abstract this to any container.
+TT T sumList(vector<T> &xs) { return accumulate(all(xs), T()); }
+
+/*
+ * verify if a `predicate` is valid for
+ * values in range [l, r]
+ *
+ * TODO: put a type in predicate ??
+ * */
+// bool all_of_in_range(T l, T r, auto predicate) {
+//     return ranges::all_of(views::iota(l, r + 1),
+//                           [&](T v) { return predicate(v); });
+// }
+
+// checks if a  value v is the interval [l, r]
+TT bool inline between(T v, T l, T r) { return clamp(v, l, r) == v; }
+
+/*8<============~ END TEMPLATE ~============}>8*/
+void __init();
+void __run();
+void __pre_run();
+
+int32_t main() {
+#ifndef LOCAL
+    fastio;
+#endif
+    __init();
+
+    int T = 1;
+
+    rep(t, 0, T) {
+        dbg(t);
+        __pre_run();
+        __run();
+    }
+}
+
+void __init() {}
+
+void __pre_run() {}
+
+ll m;
+
+struct mod_int {
+    ll x;
+
+    mod_int(ll v = 0) {
+        x = v % m;
+        if (x < 0) v += m;
+    }
+
+    mod_int &operator+=(mod_int const &b) {
+        x += b.x;
+        if (x >= m) x -= m;
+        return *this;
+    }
+
+    mod_int &operator-=(mod_int const &b) {
+        x -= b.x;
+        if (x < 0) x += m;
+        return *this;
+    }
+    mod_int &operator*=(mod_int const &b) {
+        x = (ll)x * b.x % m;
+        return *this;
+    }
+
+    friend mod_int mpow(mod_int a, ll e) {
+        mod_int res = 1;
+        while (e) {
+            if (e & 1) res *= a;
+            a *= a;
+            e >>= 1;
+        }
+        return res;
+    }
+    friend mod_int inverse(mod_int a) { return mpow(a, m - 2); }
+
+    mod_int &operator/=(mod_int const &b) { return *this *= inverse(b); }
+    friend mod_int operator+(mod_int a, mod_int const b) { return a += b; }
+    mod_int operator++(int) { return this->x = (this->x + 1) % m; }
+    mod_int operator++() { return this->x = (this->x + 1) % m; }
+    friend mod_int operator-(mod_int a, mod_int const b) { return a -= b; }
+    friend mod_int operator-(mod_int const a) { return 0 - a; }
+    mod_int operator--(int) { return this->x = (this->x - 1 + m) % m; }
+
+    mod_int operator--() { return this->x = (this->x - 1 + m) % m; }
+    friend mod_int operator*(mod_int a, mod_int const b) { return a *= b; }
+    friend mod_int operator/(mod_int a, mod_int const b) { return a /= b; }
+    friend ostream &operator<<(ostream &os, mod_int const &a) {
+        return os << a.x;
+    }
+    friend bool operator==(mod_int const &a, mod_int const &b) {
+        return a.x == b.x;
+    }
+    friend bool operator!=(mod_int const &a, mod_int const &b) {
+        return a.x != b.x;
+    }
+};
+
+void __run() {
+    ll n;
+    cin >> n >> m;
+
+    vector<mod_int> pows(n + 1, 1);
+    rep(i, 1, n + 1) { pows[i] = pows[i - 1] * n; }
+
+    mod_int ans = 0;
+
+    mod_int p = 1;
+    rep(i, 1, n + 1) {
+        ll s = (i * (i - 1)) / 2;
+        ans += p * s * pows[n - i];
+        p *= (n - i);
+    }
+
+    cout << ans * n << endl;
+}
