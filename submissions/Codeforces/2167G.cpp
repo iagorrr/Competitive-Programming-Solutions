74a514b29311728e218444da89c1e3c8be0d82a8
iagorrr <iagorrr04@gmail.com>
2026-03-24 09:06:49 -0300

    add solution for codeforces 2167G

---
+++ submissions/Codeforces/2167G.cpp
@@ -0,0 +1,182 @@
+/*8<{==========~ BEGIN TEMPLATE ~============>8*/
+#include <bits/stdc++.h>
+#include <functional>
+#include <numeric>
+using namespace std;
+#ifdef LOCAL
+#include "debug.cpp"
+#else
+#define dbg(...)
+#endif
+#define fastio                                                                 \
+  ios_base::sync_with_stdio(0);                                                \
+  cin.tie(0);
+#define all(j) j.begin(), j.end()
+#define rall(j) j.rbegin(), j.rend()
+#define len(j) (int)j.size()
+#define rep(i, a, b)                                                           \
+  for (common_type_t<decltype(a), decltype(b)> i = (a); i < (b); i++)
+#define rrep(i, a, b)                                                          \
+  for (common_type_t<decltype(a), decltype(b)> i = (a); i > (b); i--)
+#define repn(i, b) for (auto i = 0; i < (b); i++)
+#define trav(xi, xs) for (auto &xi : xs)
+#define rtrav(xi, xs) for (auto &xi : ranges::views::reverse(xs))
+#define loop while (1)
+using ll = long long;
+#define endl '\n'
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define eb emplace_back
+#define ef emplace_back
+#define lb lower_bound
+#define ub upper_bound
+#define fi first
+#define se second
+#define emp emplace
+#define ins insert
+#define divc(a, b) ((a) + (b) - 1ll) / (b)
+using str = string;
+using ull = unsigned long long;
+using ld = long double;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vll2d = vector<vll>;
+using vi = vector<int>;
+using vi2d = vector<vi>;
+using pii = pair<int, int>;
+using vpii = vector<pii>;
+using vc = vector<char>;
+using vs = vector<str>;
+
+#define TT template <typename T>
+#define TTU template <typename T, typename U>
+
+TTU using umap = unordered_map<T, U>;
+TT using pqmn = priority_queue<T, vector<T>, greater<T>>;
+TT using pqmx = priority_queue<T, vector<T>>;
+
+TTU inline bool chmax(T &a, U const &b) { return (a < b ? a = b, 1 : 0); }
+TTU inline bool chmin(T &a, U const &b) { return (a > b ? a = b, 1 : 0); }
+
+// read vector
+// TODO: abstract this to any container.
+TT std::istream &operator>>(std::istream &is, std::vector<T> &vec) {
+  for (auto &element : vec) {
+    is >> element;
+  }
+  return is;
+}
+
+// print vector
+// TODO: abstract this to any container.
+TT ostream &operator<<(ostream &os, vector<T> &xs) {
+  rep(i, os.iword(0), xs.size()) os << xs[i] << (i == xs.size() ? "" : " ");
+  os.iword(0) = 0;
+  return os;
+}
+
+// print multiset
+// TODO: abstract this to any container.
+TT ostream &operator<<(ostream &os, multiset<T> &xs) {
+  for (auto it = xs.begin(); it != xs.end(); it++)
+    os << *it << (next(it) == end(xs) ? "" : " ");
+  return os;
+}
+
+// sum a vector, using the default constructor as initial value
+// TODO: abstract this to any container.
+TT T sumList(vector<T> &xs) { return accumulate(all(xs), T()); }
+
+/*
+ * verify if a `predicate` is valid for
+ * values in range [l, r]
+ *
+ * TODO: put a type in predicate ??
+ * */
+// bool all_of_in_range(T l, T r, auto predicate) {
+//     return ranges::all_of(views::iota(l, r + 1),
+//                           [&](T v) { return predicate(v); });
+// }
+
+// checks if a  value v is the interval [l, r]
+TT bool inline between(T v, T l, T r) { return clamp(v, l, r) == v; }
+
+/*8<============~ END TEMPLATE ~============}>8*/
+void __init();
+void __run();
+void __pre_run();
+
+int32_t main() {
+#ifndef LOCAL
+  fastio;
+#endif
+  __init();
+
+  int T = 1;
+
+  cin >> T;
+
+  rep(t, 0, T) {
+    dbg(t);
+    __pre_run();
+    __run();
+  }
+}
+
+/*
+ *
+ *
+ *
+ *
+ *
+ * */
+
+void __init() {}
+
+void __pre_run() {}
+
+void __run() {
+  int n;
+  cin >> n;
+
+  vll as(n);
+  cin >> as;
+  vll cs(n);
+  cin >> cs;
+
+  if (n == 1) {
+    cout << 0 << endl;
+    return;
+  }
+
+  ll oo = accumulate(all(cs), 0ll);
+  ll ans = accumulate(all(cs), 0ll);
+
+  vll up(n, 0), here(n, oo);
+  rrep(i, n - 2, 0 - 1) {
+    here = vll(n, oo);
+    rep(j, 0, n) {
+
+      auto &ret = here.at(j);
+      ll cur = as.at(j);
+      ll nxt = as.at(i + 1);
+      if (nxt >= cur) {
+        chmin(ret, up.at(i + 1));
+        chmin(ret, up.at(j) + cs.at(i + 1));
+      } else {
+        chmin(ret, up.at(j) + cs.at(i + 1));
+      }
+    }
+    swap(here, up);
+  }
+
+  repn(i, n) chmin(ans, up.at(i) + (i == 0 ? 0 : cs.at(0)));
+  cout << ans << endl;
+}
+
+/*8<
+ 1 5 3  4 2  6 7
+10 1 10 1 10 1 10
+>8*/
